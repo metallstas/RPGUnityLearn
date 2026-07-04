8641a5f2887017d0bc4c659bf34cf25f0be2cf23
Stas Khmylko <metallstas@mail.ru>
2026-07-04 15:38:57 +0200

    components in Unity

---
+++ Assets/Player.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal"), rb.linearVelocityY);
+    }
+}
