8793f1327de05f4bc79367d6269d5250a9ba1875
Stas Khmylko <metallstas@mail.ru>
2026-07-11 14:33:07 +0200

    move and jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,6 +2,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  
+    private Rigidbody2D rb;
+
+    private float xInput;
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8f;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleMovemant();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
+    }
+
+    private void HandleMovemant()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
 
 }
